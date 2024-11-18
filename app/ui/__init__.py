fabfb17974e4129ea5306910d7080a45e48a21f9
elambert42 <elambert42@tntech.edu>
2024-11-18 13:44:41 -0600

    UI init created

---
+++ app/ui/__init__.py
@@ -0,0 +1,6 @@
+from .SmallOverlay import SmallOverlayWindow
+from .MainPage import CompletedTasksWindow
+from .EditTaskPage import AddTasksWindow
+from .CommitHistoryPage import CompleteTaskWindow
+from .CompletionPage import EditTasksWindow
+from .AddTaskPage import CommitHistoryWindow
