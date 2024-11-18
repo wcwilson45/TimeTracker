4670249a976c3f8cb17fb2dd2745c62ee8226f88
wcwilson45 <111925591+wcwilson45@users.noreply.github.com>
2024-11-18 13:51:26 -0600

    Update MainPage.py

---
+++ app/ui/MainPage.py
@@ -0,0 +1,41 @@
+from tkinter import *
+from tkinter.ttk import *
+from tkinter import filedialog
+from tkinter import ttk
+import tkinter as tk
+
+
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
+
+        self.geometry('540x1080')
+        self.title('Full Page')
+
+        menubar = Menu(self)
+        self.config(menu=menubar)
+
+        dropdown = Menu(menubar, tearoff=0)
+        menubar.add_cascade(label="...", menu=dropdown)
+        dropdown.add_command(label="Small Overlay", command=lambda: open_smalloverlaywindow(self))
+        dropdown.add_command(label="Full Page", command = lambda: open_fullpagewindow(self))
+        dropdown.add_command(label="Completed Tasks", command = lambda: open_completedtaskswindow(self))
+        dropdown.add_command(label="Import", command=ImportCommand)
+        dropdown.add_command(label="Export", command=ExportCommand)
+
+class FullPageWindow(tk.Toplevel):
+    def __init__(self, parent):
+        super().__init__(parent)
+
+        self.geometry("540x1080")
+        self.title("Full Page")
+
+        ttk.Button(self, text='Close', command=self.destroy).pack(expand=True)
+
+def open_fullpagewindow(parent):
+    window = FullPageWindow(parent)
+    window.grab_set()
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
