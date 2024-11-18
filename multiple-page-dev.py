35bf218155f3ba6e95280e5332c89f69bc26a03a
wcwilson45 <111925591+wcwilson45@users.noreply.github.com>
2024-11-18 13:48:48 -0600

    Update multiple-page-dev.py

--- multiple-page-dev.py
+++ multiple-page-dev.py
@@ -1,98 +1,86 @@
 from tkinter import *
 from tkinter.ttk import *
-from time import strftime
 from tkinter import filedialog
 from tkinter import ttk
 import tkinter as tk
 
-
-class SmallOverlayWindow(tk.Toplevel):
-    def __init__(self,parent):
-        super().__init__(parent)
-
-        self.geometry('300x150')
-        self.title('SmallOverlay')
-
-        ttk.Button(self, text='Close', command=self.destroy).pack(expand=True)
-
-class CompletedTasksWindow(tk.Toplevel):
-    def __init__(self,parent):
-        super().__init__(parent)
-
-        self.geometry('540x1080')
-        self.title('Completed Tasks')
-
-        ttk.Button(self, text = 'Close', command = self.destroy).pack(expand = True)
-
-class AddTasksWindow(tk.Toplevel):
-    def __init__(self,parent):
-        super().__init__(parent)
-
-        self.geometry('560x280')
-        self.title('Add Task')
-
-        ttk.Button(self, text = 'Close', command = self.destroy).pack(expand = True)
-
-class CompleteTaskWindow(tk.Toplevel):
-    def __init__(self,parent):
-        super().__init__(parent)
-
-        self.geometry('560x280')
-        self.title('Complete Task')
-
-        ttk.Button(self, text = 'Close', command = self.destroy).pack(expand = True)
-
-class EditTasksWindow(tk.Toplevel):
-    def __init__(self,parent):
-        super().__init__(parent)
-
-        self.geometry('560x280')
-        self.title('Edit Task')
-
-        ttk.Button(self, text = 'Close', command = self.destroy).pack(expand = True)
-
-class CommitHistoryWindow(tk.Toplevel):
-    def __init__(self,parent):
-        super().__init__(parent)
-
-        self.geometry('560x280')
-        self.title('Commit History')
-
-        ttk.Button(self, text = 'Close', command = self.destroy).pack(expand = True)
 
 class App(tk.Tk):
     def __init__(self):
         super().__init__()
 
         self.geometry('540x1080')
-        self.title('Main Window')
+        self.title('Full Page')
 
-        # place a button on the root window
-        ttk.Button(self, text='Small Overlay', command=self.open_smalloverlaywindow).pack(expand=True)
-        ttk.Button(self, text= 'Completed Tasks', command = self.open_completedtaskswindow).pack(expand = True)
-        ttk.Button(self, text= 'Edit Task', command = self.open_edittaskswindow).pack(expand = True)
-        ttk.Button(self, text= 'Add Task', command = self.open_addtaskswindow).pack(expand = True)
-        ttk.Button(self, text= 'Commit History', command = self.open_commithistorywindow).pack(expand = True)
-        ttk.Button(self, text= 'Complete Task', command = self.open_completetaskwindow).pack(expand = True)
+        menubar = Menu(self)
+        self.config(menu=menubar)
 
-    def open_smalloverlaywindow(self):
-        window = SmallOverlayWindow(self)
-        window.grab_set()
-    def open_completedtaskswindow(self):
-        window = CompletedTasksWindow(self)
-        window.grab_set()
-    def open_edittaskswindow(self):
-        window = EditTasksWindow(self)
-        window.grab_set()
-    def open_addtaskswindow(self):
-        window = AddTasksWindow(self)
-        window.grab_set()
-    def open_commithistorywindow(self):
-        window = CommitHistoryWindow(self)
-        window.grab_set()
-    def open_completetaskwindow(self):
-        window = CompleteTaskWindow(self)
-        window.grab_set()
+        dropdown = Menu(menubar, tearoff=0)
+        menubar.add_cascade(label="...", menu=dropdown)
+        dropdown.add_command(label="Small Overlay", command=lambda: open_smalloverlaywindow(self))
+        dropdown.add_command(label="Full Page", command = lambda: open_fullpagewindow(self))
+        dropdown.add_command(label="Completed Tasks", command = lambda: open_completedtaskswindow(self))
+        dropdown.add_command(label="Import", command=ImportCommand)
+        dropdown.add_command(label="Export", command=ExportCommand)
+
+
+class SmallOverlayWindow(tk.Toplevel):
+    def __init__(self, parent):
+        super().__init__(parent)
+
+        self.geometry("540x320")
+        self.title("Small Overlay")
+
+        ttk.Button(self, text='Close', command=self.destroy).pack(expand=True)
+
+
+def open_smalloverlaywindow(parent):
+    window = SmallOverlayWindow(parent)
+    window.grab_set()
+
+
+class FullPageWindow(tk.Toplevel):
+    def __init__(self, parent):
+        super().__init__(parent)
+
+        self.geometry("540x1080")
+        self.title("Full Page")
+
+        ttk.Button(self, text='Close', command=self.destroy).pack(expand=True)
+
+def open_fullpagewindow(parent):
+    window = FullPageWindow(parent)
+    window.grab_set()
+
+
+
+class CompletedTasksWindow(tk.Toplevel):
+    def __init__(self, parent):
+        super().__init__(parent)
+
+        self.geometry("540x320")
+        self.title("Completed Tasks")
+
+        ttk.Button(self, text='Close', command=self.destroy).pack(expand=True)
+
+def open_completedtaskswindow(parent):
+    window = CompletedTasksWindow(parent)
+    window.grab.set()
+
+
+def ImportCommand():
+    filename = filedialog.askopenfilename(
+        initialdir="~",
+        title="Select a File (.csv)",
+        filetypes=[("CSV files", "*.csv")]
+    )
+    if filename:
+        print(f"Selected file: {filename}")
+
+
+def ExportCommand():
+    print("Export")
+
 
 if __name__ == "__main__":
     app = App()
