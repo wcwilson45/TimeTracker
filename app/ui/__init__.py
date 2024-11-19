6f44d46bcf751b79c6e4c93eac693a9205a477c7
Jake2001 <jsamh12@outlook.com>
2024-11-19 13:37:56 -0600

    Imports

--- app/ui/__init__.py
+++ app/ui/__init__.py
@@ -1,6 +1,6 @@
 from .SmallOverlay import SmallOverlayWindow
 from .MainPage import CompletedTasksWindow
-from .EditTaskPage import AddTasksWindow
+from .EditTaskPage import EditTaskWindow
 from .CommitHistoryPage import CompleteTaskWindow
 from .CompletionPage import EditTasksWindow
 from .AddTaskPage import CommitHistoryWindow
