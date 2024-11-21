990800d2556775ee4d9f98e1f27387b37cbd9d0a
Jake2001 <jsamh12@outlook.com>
2024-11-21 12:52:47 -0600

    Changed colors and font

--- app/ui/EditTaskPage.py
+++ app/ui/EditTaskPage.py
@@ -8,30 +8,29 @@ class EditTaskWindow(tk.Tk):  # Inherit from tk.Tk to make it a standalone app
 
         self.geometry("620x400")
         self.title("Edit Task")
-        self.configure(bg='#444444')
-
+        self.configure(bg='#f0f0f0')
 
         self.style = ttk.Style()
         self.style.theme_use('alt')
-        self.style.configure("Custom.TCombobox", fieldbackground="light blue", background="light blue", arrowcolor='black'
-                             , foreground='black',  font=("SF Pro", 12))
-        self.style.configure("Custom.TLabel", background='#444444', foreground='#FFFFFF', font=("SF Pro", 12))
+        self.style.configure("Custom.TCombobox", fieldbackground="white", background="#f0f0f0", arrowcolor='black'
+                             , foreground='black',  font=("SF Pro Display", 12))
+        self.style.configure("Custom.TLabel", background='#f0f0f0', foreground='black', font=("SF Pro Display", 12, 'bold'))
 
         # Task Name
         ttk.Label(self, text="Task Name:", style="Custom.TLabel").grid(row=0, column=0, sticky="w", padx=20, pady=10)
-        tk.Entry(self, width=40, bg='light blue', font=("SF Pro ", 12)).grid(row=0, column=1, padx=2, pady=2, sticky='W')
+        tk.Entry(self, width=40, bg='white', font=("SF Pro Display", 12)).grid(row=0, column=1, padx=2, pady=2, sticky='W')
 
         # Task Description
         ttk.Label(self, text="Edit Description:", style="Custom.TLabel").grid(row=1, column=0, sticky="nw", padx=20, pady=10)
-        tk.Text(self, height=5, width=40, bg='light blue', font=("SF Pro", 12)).grid(row=1, column=1, pady=10, sticky="W")
+        tk.Text(self, height=5, width=40, bg='white', font=("SF Pro Display", 12)).grid(row=1, column=1, pady=10, sticky="W")
 
         # Tags
         ttk.Label(self, text="Edit Tags:", style="Custom.TLabel").grid(row=2, column=0, sticky="w", padx=20, pady=10)
-        tk.Entry(self, width=40, bg='light blue', font=("SF Pro", 12)).grid(row=2, column=1, pady=10, sticky="W")
+        tk.Entry(self, width=40, bg='white', font=("SF Pro Display", 12)).grid(row=2, column=1, pady=10, sticky="W")
 
         # Time of Completion
         ttk.Label(self, text="Edit Time of Completion:", style="Custom.TLabel").grid(row=3, column=0, sticky="w", padx=20, pady=10)
-        tk.Entry(self, width=40, bg='light blue', font=("SF Pro", 12)).grid(row=3, column=1, pady=10, sticky="W")
+        tk.Entry(self, width=40, bg='white', font=("SF Pro Display", 12)).grid(row=3, column=1, pady=10, sticky="W")
 
         # Time Complexity
         ttk.Label(self, text="Edit Time Complexity:", style="Custom.TLabel").grid(row=4, column=0, sticky="w", padx=20, pady=10)
@@ -39,7 +38,7 @@ class EditTaskWindow(tk.Tk):  # Inherit from tk.Tk to make it a standalone app
 
         # Date Completed
         ttk.Label(self, text="Edit Date Completed:", style="Custom.TLabel").grid(row=5, column=0, sticky="w", padx=20, pady=10)
-        tk.Entry(self, width=20, bg='light blue', font=("SF Pro", 12)).grid(row=5, column=1, pady=10, sticky="w")
+        tk.Entry(self, width=20, bg='white', font=("SF Pro Display", 12)).grid(row=5, column=1, pady=10, sticky="w")
 
         # Buttons at Bottom-Right
         confirm_button = tk.Button(self, text="Confirm", width=15, bg='#90EE90', fg='black')
@@ -48,3 +47,8 @@ class EditTaskWindow(tk.Tk):  # Inherit from tk.Tk to make it a standalone app
         exit_button = tk.Button(self, text="Exit", width=15, bg='#F08080', fg='black', command=self.destroy)
         exit_button.grid(row=6, column=1, pady=20, sticky="e")
 
+
+if __name__ == "__main__":
+
+    app = EditTaskWindow()
+    app.mainloop()
