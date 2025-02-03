795a0fa27d9c4fe5422a4253bc647740332fe1a1
Jake2001 <jsamh12@outlook.com>
2025-02-02 20:40:01 -0600

    Completed the UI and functionality for the Tags DB

--- app/ui/TagsDB.py
+++ app/ui/TagsDB.py
@@ -4,20 +4,19 @@ from tkinter import ttk
 import tkinter as tk
 import tkinter.font as tkfont
 import sqlite3
+import csv
 from tkinter import messagebox
 
-
-
-
-class TagsDB(tk.Toplevel):
+class TagsDB(tk.Frame):
     def __init__(self,parent):
         super().__init__(parent)
-        #DATABASE SECTION
+        #DATABASE SECTION ############################################
 
         # Create or Connect to database
+        
         conn = sqlite3.connect("tags.db")
 
-        #Create a cursor instnace
+        # Create a cursor instnace
         c = conn.cursor()
 
         c.execute("""
@@ -71,10 +70,12 @@ class TagsDB(tk.Toplevel):
 
             for tag in tags:
                 if count % 2 == 0:
-                    my_tree.insert(parent='', index='end', iid=count, text='', values=(tag[0], tag[2], tag[3]), tags=('evenrow',))
+                    # For even rows (0, 2, 4, ...) apply 'evenrow' tag
+                    my_tree.insert(parent='', index='end', iid=count, text='', values=(tag[0], tag[2], tag[3]), tags=('evenrow',""))
                 else:
-                    my_tree.insert(parent='', index='end', iid=count, text='', values=(tag[0], tag[2], tag[3]), tags=('oddrow',))
-
+                    # For odd rows (1, 3, 5, ...) apply 'oddrow' tag
+                    my_tree.insert(parent='', index='end', iid=count, text='', values=(tag[0], tag[2], tag[3]), tags=('oddrow',""))
+                
                 # Increment counter
                 count += 1
 
@@ -84,17 +85,11 @@ class TagsDB(tk.Toplevel):
             # Close connection to the database
             conn.close()
 
-        #END OF DATABASE SECTION
+        #END OF DATABASE SECTION #################################################
 
-        # Main container
-        main_frame = tk.Frame(self, bg='#5DADE2', bd=0)
-        main_frame.grid(row=0, column=0, padx=10, pady=5, sticky="nsew")
 
-        # Set the main window geometry and title
-        self.geometry("660x620")  
-        self.title("Tag DataBase")
+        # Set the main window 
         self.configure(bg='#5DADE2')
-        self.resizable(False, False)
 
         # Create fonts
         self.fonts = {
@@ -104,23 +99,43 @@ class TagsDB(tk.Toplevel):
         }
 
         # Style configurations
-        self.style = ttk.Style(self)
-        self.style.theme_use("alt")  
-        self.style.configure('Input.TEntry', fieldbackground='#d3d3d3', font=("SF Pro Text", 10))
-        self.style.configure('TLabel', background='#d3d3d3', font=("SF Pro Text", 10))  
-        self.style.configure('TButton', background='#5DADE2', font=("SF Pro Text", 10))
+        style = ttk.Style(self)
+        style.theme_use("alt")  
+        style.configure('Input.TEntry', fieldbackground='#d3d3d3', font=("SF Pro Text", 10))
+        style.configure('TLabel', background='#d3d3d3', font=("SF Pro Text", 10))  
+        style.configure('TButton', background='#5DADE2', font=("SF Pro Text", 10))
 
         # Create a Treeview Frame
-        tree_frame = tk.Frame(main_frame, bg='#D3D3D3')
-        tree_frame.grid(row=0, column=0, padx=1, pady=1, columnspan=4, sticky="nsew")
+        tree_frame = tk.Frame(self, bg='#5DADE2')
+        tree_frame.grid(row=1, column=0, sticky="nsew")
+
+        # Change color when a item is selected
+        style.map("Treeview",
+        background = [('selected', "347083")])
+
+        style.configure("Treeview",
+        background = "black",
+        foreground = "black",
+        rowheight = 25,
+        fieldbackground = "#d3d3d3",
+        bd = "black")
+
+
+        # Ensure that tree_frame grid is configured properly
+        tree_frame.grid_rowconfigure(0, weight=1)  # Allow row 0 to expand
+        tree_frame.grid_columnconfigure(0, weight=1)  # Allow column 0 to expand
 
         # Create a Treeview Scrollbar
         tree_scroll = Scrollbar(tree_frame)
-        tree_scroll.pack(side=RIGHT, fill=Y)
+        tree_scroll.grid(row = 1, column = 1, sticky = "ns")
 
         # Create The Treeview
-        my_tree = ttk.Treeview(tree_frame, yscrollcommand=tree_scroll.set, selectmode="extended")
-        my_tree.pack()
+        my_tree = ttk.Treeview(tree_frame, yscrollcommand=tree_scroll.set, selectmode="extended",style="Treeview")
+        my_tree.grid(row=1, column=0, sticky="nsew")
+
+        # Configure the different rows for color
+        my_tree.tag_configure('oddrow', background=  "#A9A9A9", foreground= "black")
+        my_tree.tag_configure('evenrow', background= "#d3d3d3", foreground= "black")
 
         # Configure the Scrollbar
         tree_scroll.config(command=my_tree.yview)
@@ -139,32 +154,33 @@ class TagsDB(tk.Toplevel):
         my_tree.heading("ID", text="ID", anchor=W)
         my_tree.heading("Name", text="Name", anchor=W)
         my_tree.heading("Description", text="Description", anchor=W)
-
-        data_frame = LabelFrame(self, text="Tag")
-        data_frame.grid(row=1, column=0, padx=10, pady=10, columnspan=4, sticky="nsew")
+        
+        
+        data_frame = tk.Frame(self,background="#5DADE2")
+        data_frame.grid(row=2, column=0, sticky="w")
 
      
         # ID Label
-        id_label = Label(data_frame, text="ID", font=("SF Pro Text", 10))
-        id_label.grid(row=0, column=0, padx=1, pady=5, sticky="w")
+        id_label = ttk.Label(data_frame, text="ID: ", font=("SF Pro Text", 10, "bold"), background="#5DADE2")
+        id_label.grid(row=0, column=0, pady=1, sticky="w")
 
         # Change the ID entry to a Label to hold the ID (non-editable)
-        id_display = Label(data_frame, font=("SF Pro Text", 10))
-        id_display.grid(row=1, column=0, padx=1, pady=5, sticky="w")
+        id_display = ttk.Label(data_frame, font=("SF Pro Text", 10, "bold"), background="#5DADE2")
+        id_display.grid(row=0, column=0, padx=20, pady=3, sticky="w")
 
         # Tag Name Label & Entry
-        n_label = Label(data_frame, text="Tag Name", font=("SF Pro Text", 10))
-        n_label.grid(row=2, column=0, padx=1, pady=5, sticky="w")
-        n_entry = Entry(data_frame, font=("SF Pro Text", 10))
-        n_entry.grid(row=3, column=0, padx=1, pady=5, sticky="w")
+        n_label = ttk.Label(data_frame, text="Tag Name", font=("SF Pro Text", 10, "bold"),background="#5DADE2")
+        n_label.grid(row=3, column=0, padx=1, pady=5, sticky="w")
+        n_entry = tk.Entry(data_frame, font=("SF Pro Text", 10), bg="#d3d3d3")
+        n_entry.grid(row=4, column=0, padx=1, pady=1, sticky="w")
 
         # Description Label & Textbox
-        desc_label = Label(data_frame, text="Description", font=("SF Pro Text", 10))
-        desc_label.grid(row=4, column=0, padx=1, pady=5, sticky="w")
-        desc_text = Text(data_frame, font=("SF Pro Text", 10), height=5, width=40)
-        desc_text.grid(row=5, column=0, padx=1, pady=5, sticky="w")
+        desc_label = Label(data_frame, text="Description", font=("SF Pro Text", 10, "bold"),background="#5DADE2")
+        desc_label.grid(row=5, column=0, padx=1, pady=5, sticky="w")
+        desc_text = Text(data_frame, font=("SF Pro Text", 12), height=5, width=40, background="#d3d3d3")
+        desc_text.grid(row=6, column=0, padx=1, pady=1, sticky="w")
 
-        # START OF FUNCTION SECTION
+        # START OF FUNCTION SECTION ##############################################
 
         # Clear entry boxes
         def clear_Tag():
@@ -256,21 +272,6 @@ class TagsDB(tk.Toplevel):
 
                 
 
-            
-
-        # Move Row Up
-        def up():
-            rows = my_tree.selection()
-            for row in rows:
-                my_tree.move(row, my_tree.parent(row), my_tree.index(row)-1)
-
-        # Move Row Down
-        def down():
-            rows = my_tree.selection()
-            for row in reversed(rows):
-                my_tree.move(row, my_tree.parent(row), my_tree.index(row)+1)
-
-
         # Add Tag to db
         def add_Tag():
             #Create db connection
@@ -331,65 +332,195 @@ class TagsDB(tk.Toplevel):
             desc_text.delete("1.0", END)
             id_display.configure(text="")
 
+
+        def search_Tag(event):
+
+            #Getting the name they entered
+            lookup = search_entry.get()
+
+            # Create or Connect to the database
+            conn = sqlite3.connect('tags.db')
+
+            # Create a cursor instance
+            c = conn.cursor()
+
+            # Clear the Treeview
+            for tag in my_tree.get_children():
+                my_tree.delete(tag)
+
+            if lookup == "":
+
+                c.execute("SELECT rowid, * FROM tags")  # Fetch all rows
+            else:
+                c.execute("SELECT rowid, * FROM tags WHERE tag_name like ?", (f"%{lookup}%",)) 
+                
+            tags = c.fetchall()
             
 
-        #END OF FUNCTION SECTION
+            # Add data to the screen
+            global count
+            count = 0
+
+            for tag in tags:
+                if count % 2 == 0:
+                    my_tree.insert(parent='', index='end', iid=count, text='', values=(tag[0], tag[2], tag[3]), tags=('evenrow',""))
+                else:
+                    my_tree.insert(parent='', index='end', iid=count, text='', values=(tag[0], tag[2], tag[3]), tags=('oddrow',""))
+
+                # Increment counter
+                count += 1
+
+            # Commit changes
+            conn.commit()
+
+            # Close connection to the database
+            conn.close()
+            
+
+
+
+        def import_Tags():
+            global data
+            # Ask user for the file
+            file_path = tk.filedialog.askopenfilename(filetypes=[("CSV Files", "*.csv")])
+
+            # If file has been selected cont
+            if file_path:
+                # Open file and read from file
+                with open(file_path, "r", encoding="utf-8-sig") as file:
+                    csv_reader = csv.reader(file)
+                    data = list(csv_reader)
+
+                    # Check if the first row is a header (by comparing column names)
+                    if data:
+                        header = data[0]
+                        expected_header = ["Tag Name", "Description"]
+
+                        # If the first row matches the header, remove it
+                        if header == expected_header:
+                            data = data[1:]
+            
+            # Connect to the SQLite database
+            conn = sqlite3.connect('tags.db')
+            c = conn.cursor()
+
+            # Insert data into the table
+            c.executemany("INSERT INTO tags (tag_name, description) VALUES (?, ?)", data)
+
+            # Commit the changes and close the connection
+            conn.commit()
+            conn.close()
+
+            query_database()
+
+
+        def export_Tags():
+            # Connect to the database
+            conn = sqlite3.connect('tags.db')
+            c = conn.cursor()
+
+            # Fetch all tags
+            c.execute("SELECT tag_name, description FROM tags")
+            tags = c.fetchall()
+
+            # Ask user where to save the file
+            file_path = tk.filedialog.asksaveasfilename(defaultextension=".csv", filetypes=[("CSV Files", "*.csv")])
+
+            if file_path:  # Only proceed if the user selected a file path
+                # Open the CSV file for writing
+                with open(file_path, "w", newline='', encoding='utf-8') as file:
+                    csv_writer = csv.writer(file)
+
+                    # Write the header
+                    csv_writer.writerow(["Tag Name", "Description"])
+
+                    # Write data rows
+                    for tag in tags:
+                        csv_writer.writerow(tag)
+
+                # Close the database connection
+                conn.close()
+            
+
+
+                    
+        #END OF FUNCTION SECTION ########################################
+
+        # Search Bar
+
+        # Frame for search bar
+        search_frame = tk.Frame(self, bg='#5DADE2')
+        search_frame.grid(row=0, column=0, padx=1, pady=1, sticky="ew")
+        
+        # Search bar label
+        search_label = Label(search_frame, text="Search by Tag Name:", font=("SF Pro Text", 10, "bold"), background="#5DADE2")
+        search_label.grid(row=0, column=0, padx=5, pady=5, sticky="w")
+
+        # Entry for search bar for user to input search
+        search_entry = tk.Entry(search_frame, font=("SF Pro Text", 10), bg="#d3d3d3")
+        search_entry.grid(row=0, column=1, padx=5, pady=5, sticky="ew")
+        search_entry.bind("<KeyRelease>", search_Tag)
+
 
         # Bind the treeview
         my_tree.bind("<ButtonRelease-1>", select_tag)
 
-        # Frame for the buttons
-        button_frame = tk.Frame(data_frame)
-        button_frame.grid(row=6, column=0, columnspan=4, pady=5, padx=5, sticky="w")
+       # Frame for the buttons
+        button_frame = tk.Frame(self, background="#5DADE2")
+        button_frame.grid(row=7, column=0, pady=1, padx=1, sticky="w")
 
-        # Configure columns of the button_frame to prevent stretching
-        button_frame.grid_columnconfigure(0, weight=0, minsize=80)  # Set a smaller minimum size for each column
-        button_frame.grid_columnconfigure(1, weight=0, minsize=80)
-        button_frame.grid_columnconfigure(2, weight=0, minsize=80)
-        button_frame.grid_columnconfigure(3, weight=0, minsize=80)
+        # Configure columns of the button_frame to distribute the space evenly
+        button_frame.grid_columnconfigure(0, weight=1)
+        button_frame.grid_columnconfigure(1, weight=1)
+        button_frame.grid_columnconfigure(2, weight=1)
+        button_frame.grid_columnconfigure(3, weight=1)
+        button_frame.grid_columnconfigure(4, weight=1)
+        button_frame.grid_columnconfigure(5, weight=1)
+        button_frame.grid_columnconfigure(6, weight=1)
 
         # Add Tag Button
-        add_btn = tk.Button(button_frame, text="Add Tag",  
-                            bg="#90EE90", fg="#000000", font=("SF Pro Text", 10),command= add_Tag,
+        add_btn = tk.Button(button_frame, text=" Add Tag ",  
+                            bg="#90EE90", fg="#000000", font=("SF Pro Text", 10), command=add_Tag,
                             relief="flat", activebackground="#A8F0A8", activeforeground="#000000")
-        add_btn.grid(row=0, column=0, pady=2, padx=3, sticky="w")
+        add_btn.grid(row=0, column=0, pady=2, padx=(0, 3), sticky="w")
 
-        # Delete Tag Button
-        del_btn = tk.Button(button_frame, text="Delete Tag", command=del_Tag,
+        # Update Button
+        update_btn = tk.Button(button_frame, text="Update Tag", command=update_Tag,
                             bg="#90EE90", fg="#000000", font=("SF Pro Text", 10),
                             relief="flat", activebackground="#A8F0A8", activeforeground="#000000")
-        del_btn.grid(row=0, column=1, pady=2, padx=3, sticky="w")
-
+        update_btn.grid(row=0, column=1, pady=2, padx=3, sticky="ew")
 
         # Clear Task Button
         clc_btn = tk.Button(button_frame, text="Clear Task", command=clear_Tag,
                             bg="#90EE90", fg="#000000", font=("SF Pro Text", 10),
                             relief="flat", activebackground="#A8F0A8", activeforeground="#000000")
-        clc_btn.grid(row=0, column=2, pady=2, padx=3, sticky="w")
+        clc_btn.grid(row=0, column=2, pady=2, padx=3, sticky="ew")
 
-        # Move Up Button
-        up_btn = tk.Button(button_frame, text="Move Up", command=up,
-                        bg="#90EE90", fg="#000000", font=("SF Pro Text", 10),
-                        relief="flat", activebackground="#A8F0A8", activeforeground="#000000")
-        up_btn.grid(row=0, column=3, pady=2, padx=3, sticky="w")
 
-        # Move Down Button
-        dwn_btn = tk.Button(button_frame, text="Move Down", command=down,
-                            bg="#90EE90", fg="#000000", font=("SF Pro Text", 10),
-                            relief="flat", activebackground="#A8F0A8", activeforeground="#000000")
-        dwn_btn.grid(row=0, column=4, pady=2, padx=3, sticky="w")
+        # Delete Tag Button
+        del_btn = tk.Button(button_frame, text="Delete Tag", command=del_Tag,
+                            bg="#FFA500", fg="#000000", font=("SF Pro Text", 10),
+                            relief="flat", activebackground="#FFB347", activeforeground="#000000")
+        del_btn.grid(row=0, column=5, pady=2, padx=(100, 3), sticky="ew")
 
         # Remove All Button
-        dwn_btn = tk.Button(button_frame, text="Remove All", command=remove_all_Tags,
-                            bg="#90EE90", fg="#000000", font=("SF Pro Text", 10),
-                            relief="flat", activebackground="#A8F0A8", activeforeground="#000000")
-        dwn_btn.grid(row=0, column=5, pady=2, padx=3, sticky="w")
+        remove_all_btn = tk.Button(button_frame, text="Remove All", command=remove_all_Tags,
+                                    bg="#FFA500", fg="#000000", font=("SF Pro Text", 10),
+                                    relief="flat", activebackground="#FFB347", activeforeground="#000000")
+        remove_all_btn.grid(row=0, column=6, pady=2, padx=3, sticky="ew")
 
-        # Update Button
-        dwn_btn = tk.Button(button_frame, text="Update Tag", command=update_Tag,
-                            bg="#90EE90", fg="#000000", font=("SF Pro Text", 10),
-                            relief="flat", activebackground="#A8F0A8", activeforeground="#000000")
-        dwn_btn.grid(row=0, column=6, pady=2, padx=3, sticky="w")
+
+        # Import Button
+        import_btn = tk.Button(search_frame, text=" Import ", command=import_Tags,
+                                    bg="#90EE90", fg="#000000", font=("SF Pro Text", 10),
+                                    relief="flat", activebackground="#A8F0A8", activeforeground="#000000")
+        import_btn.grid(row=0, column=2, pady=3, padx=(85,3), sticky="w")
+
+        # Import Button
+        export_btn = tk.Button(search_frame, text=" Export ", command=export_Tags,
+                                    bg="#90EE90", fg="#000000", font=("SF Pro Text", 10),
+                                    relief="flat", activebackground="#A8F0A8", activeforeground="#000000")
+        export_btn.grid(row=0, column=2, pady=3, padx=(145,3), sticky="e")
 
         # Run to pull data from database on start
         query_database()
